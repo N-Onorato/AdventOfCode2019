b66973746762e8eeb70f514d463d5e419867ab4f
Nick <n_onorato@outlook.com>
2019-12-03 10:01:13 -0700

    initial commit

---
+++ SolutionTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
